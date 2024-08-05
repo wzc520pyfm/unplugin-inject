b69eec92625f0fef68425927650d776c09de4136
wzc520pyfm <1528857653@qq.com>
2024-08-05 23:46:00 +0800

    feat: add inject

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+/**
+ * A valid `picomatch` glob pattern, or array of patterns.
+ */
+export type FilterPattern = ReadonlyArray<string | RegExp> | string | RegExp | null
+
 export interface Options {
   // define your plugin options here
+  include?: FilterPattern
+  exclude?: FilterPattern
+  [key: string]: any
 }
